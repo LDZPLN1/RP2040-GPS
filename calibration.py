133593b6c00749b680aee3df768713962205f35c
Douglas Graham <ldzpln1@aol.com>
2024-02-28 01:17:36 -0500

    Test

--- calibration.py
+++ calibration.py
@@ -71,7 +71,7 @@ disp_group.append(tile_grid)
 disp.show(disp_group)
 
 comp_angle = (11.25, 33.75, 56.25, 78.75, 101.25, 123.75, 146.25, 168.75, 191.25, 213.75, 236.25, 258.75, 281.25, 303.75, 326.25, 348.75)
-comp_point = ('NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NN')
+comp_point = ('NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW')
 
 font = terminalio.FONT
 
